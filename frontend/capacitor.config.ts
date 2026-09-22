154e8fea7fb129e16dd59d0fd03f9d5c95df72cc
benjahern <benjamin.hern.m@gmail.com>
2026-09-22 10:57:18 -0300

    v0.1.1: Actualizacion para servidores aarm

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'cl.3dcotization.frontend',
-  appName: 'frontend',
+  appName: 'frontend',cle
   webDir: 'dist'
 };
 
